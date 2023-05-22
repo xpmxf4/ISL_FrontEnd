aec527f26af7135eb6072668c1603daf0cece2b5
dev-chpark <xpmxf5@gmail.com>
2023-05-22 14:15:11 +0900

    ⭐ feat: 메인 페이지 레이아웃 완성 / 사이드바 UI + 토글 완성

---
+++ src/js/sidebar.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggle');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+const worldMap = document.getElementById('worldMap');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('open');
+    overlay.classList.toggle('show');
+    worldMap.classList.toggle('dimmed');
+}
+
+toggleButton.addEventListener('click', toggleSidebar);
+overlay.addEventListener('click', toggleSidebar);
