75869d583f7333a59ce20cd999b7f43f1df8f878
dev-chpark <xpmxf5@gmail.com>
2023-06-13 13:37:51 +0900

    ⭐ feat: 첫 방문에만 토큰 불러오도록 수정

--- src/js/bringCSRF.js
+++ src/js/bringCSRF.js
@@ -2,11 +2,13 @@
 const csrfApi = "https://ig6oli6355.execute-api.ap-northeast-2.amazonaws.com/dev";
 // const csrfApi = 'http://127.0.0.1:3000';
 
-fetch(csrfApi + "/csrf-token")
-    .then(res => res.json())
-    .then(data => {
-        localStorage.setItem('csrfToken', data.csrfToken)
-    })
-    .catch(err => {
-        console.log(err)
-    });
+if (localStorage.getItem("csrfToken") == null) {
+    fetch(csrfApi + "/csrf-token")
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem('csrfToken', data.csrfToken)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
