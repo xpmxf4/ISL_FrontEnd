4a2fce39438bc9659ff4a203bbc04bc9181dd979
dev-chpark <xpmxf5@gmail.com>
2023-06-15 15:13:39 +0900

    ⭐ feat: 법규 POST 통신 완료

--- src/js/clickCountry.js
+++ src/js/clickCountry.js
@@ -1,35 +1,4 @@
 const pathTags = document.getElementsByClassName("sm_state");
-const jsonPath = "/src/json/countries.json";
-let fromCountry = null;
-
-// Get user's country
-fetch('http://api.ipapi.com/220.70.232.230?access_key=354b20f8afd1c43661ac7b5262c9aae7')
-    .then(res => {
-        if (!res.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return res.json();
-    })
-    .then(data => {
-        return data.country_code;
-    }).then(data => {
-        fetch(jsonPath)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(res.status + " " + res.statusText);
-                }
-                return res.json();
-            })
-            .then(res => {
-                fromCountry = res[data];
-            }
-            )
-    })
-    .catch(err => {
-        console.error('There has been a problem with your fetch operation:', err);
-    });
-
-
 
 // Loop through every country elements
 for (let i = 0; i < pathTags.length; i++) {
@@ -41,21 +10,7 @@ for (let i = 0; i < pathTags.length; i++) {
         // Get el's full country name from class
         // have to get countryName from sm_state_QA
         let toCountry = el.classList[1].split("_")[2];
-
-        // Match with countries.json
-        await fetch(jsonPath)
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error(res.status + " " + res.statusText);
-                }
-                return res.json();
-            })
-            .then(data => {
-                toCountry = data[toCountry];
-            })
-            .catch(err => {
-                console.log(err);
-            })
+        console.log(toCountry)
 
         // await fetch("https://ig6oli6355.execute-api.ap-northeast-2.amazonaws.com/dev/countries", {
         await fetch("http://127.0.0.1:3000/countries", {
@@ -66,8 +21,7 @@ for (let i = 0; i < pathTags.length; i++) {
                 "CSRF-Token": localStorage.getItem("csrfToken"),
             },
             body: JSON.stringify({
-                "fromCountry": "Korea",
-                "toCountry": "United Kingdom",
+                "toCountry": toCountry,
             })
         })
             .then(res => res.json())
