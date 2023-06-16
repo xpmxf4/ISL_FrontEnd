59747a5c88ce141b8d6c81272b5e183bf6ab39e6
unknown <xpmxf5@gmail.com>
2023-06-16 13:38:28 +0900

    ⚙️ chore: fixed untracked files

--- src/js/clickCountry.js
+++ src/js/clickCountry.js
@@ -1,8 +1,11 @@
-const pathTags = Array.from(document.getElementsByClassName("sm_state"));
+import { toggleSidebar } from "./sidebar.js"
+
+const pathTags = Array.from(document.getElementsByClassName("sm_state"))
+const api = "http://127.0.0.1:3000"
 
 const fetchCountryData = async (toCountry) => {
     try {
-        const response = await fetch("http://127.0.0.1:3000/countries", {
+        const response = await fetch("http://127.0.0.1:3000" + "/countries", {
             method: "POST",
             credentials: "include",
             headers: {
@@ -10,25 +13,39 @@ const fetchCountryData = async (toCountry) => {
                 "CSRF-Token": localStorage.getItem("csrfToken"),
             },
             body: JSON.stringify({
-                "toCountry": toCountry,
-            })
+                toCountry: toCountry,
+            }),
         })
 
-        const data = await response.json();
+        const data = await response.json()
     } catch (err) {
-        console.error(`Error fetching country data: ${err}`);
+        console.error(`Error fetching country data: ${err}`)
+    }
+}
+
+// check where if it is initial visit
+const checkInitialVisit = () => {
+    const isInitialVisit = localStorage.getItem("isInitialVisit")
+
+    // if isInitialVisit===true -> set isInitialVisit =false, open sidebar
+    if (isInitialVisit == null) {
+        console.log("in if");
+        localStorage.setItem("isInitialVisit", false)
+        toggleSidebar()
     }
 }
 
 // Loop through every country elements
-pathTags.forEach(el => {
+pathTags.forEach((el) => {
     // Add event on element
-    el.addEventListener("click", async () => {
+    el.addEventListener("click", () => {
+        checkInitialVisit()
+
         // Get el's full country name from class
         // have to get countryName from sm_state_QA
-        let toCountry = el.classList[1].split("_")[2];
+        let toCountry = el.classList[1].split("_")[2]
 
         // Fetch data
-        fetchCountryData(toCountry);
+        fetchCountryData(toCountry)
     })
-});
+})
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -3,10 +3,10 @@ const sidebar = document.getElementById('sidebar');
 const overlay = document.getElementById('overlay');
 const worldMap = document.getElementById('worldMap');
 
-function toggleSidebar() {
-    sidebar.classList.toggle('open');
-    overlay.classList.toggle('show');
-    worldMap.classList.toggle('dimmed');
+export function toggleSidebar() {
+    sidebar.classList.toggle("open")
+    overlay.classList.toggle("show")
+    worldMap.classList.toggle("dimmed")
 }
 
 toggleButton.addEventListener('click', toggleSidebar);
