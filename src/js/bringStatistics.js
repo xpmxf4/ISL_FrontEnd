b62746dae7fae614eb43a6dfe43e6e41e33eb017
unknown <xpmxf5@gmail.com>
2023-06-16 16:30:30 +0900

    ⭐ feat:  통계형 사이드바 레이아웃 완성, 디자인 수정 필요

--- src/js/bringStatistics.js
+++
@@ -1,7 +0,0 @@
-const api = "http://127.0.0.1:3000";
-
-fetch(api + "/statistics/top5")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,12 +1,40 @@
 const toggleButton = document.getElementById('toggle');
 const sidebar = document.getElementById('sidebar');
 const overlay = document.getElementById('overlay');
+const statistics = document.getElementsByClassName('statistics')
 const worldMap = document.getElementById('worldMap');
+const api = "http://127.0.0.1:3000"
+
+let isOpen = false;
 
 export function toggleSidebar() {
-    sidebar.classList.toggle("open")
-    overlay.classList.toggle("show")
-    worldMap.classList.toggle("dimmed")
+    sidebar.classList.toggle("open");
+    overlay.classList.toggle("show");
+    worldMap.classList.toggle("dimmed");
+    for (let stat of statistics) {
+        stat.classList.toggle('open')
+    }
+
+    isOpen = !isOpen;
+
+    if (isOpen) {
+        fetch(api+"/statistics/top5")  // API URL로 fetch 요청
+        .then(response => response.json())  // JSON 형태로 응답을 받음
+        .then(data => {
+
+            const statisticsContainer = document.getElementById('statisticsContainer');
+            const keys = ['daily', 'weekly', 'monthly', 'all']
+
+            keys.forEach((key, index) => {
+                let html = `<p>${key.toUpperCase()}</p>`
+                data[key].forEach(el => {
+                    html += `<p>${el["to_country"]} : ${el["count"]}</p>`
+                })
+                statisticsContainer.children[index].innerHTML = html
+            });
+        })
+        .catch(error => console.error('Error:', error));
+    }
 }
 
 toggleButton.addEventListener('click', toggleSidebar);
